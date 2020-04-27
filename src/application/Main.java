d1d7f19f98077787387d8f0d52d38069e994da4f
Nilton da Silva Junior <tonsti@gmail.com>
2020-04-27 20:06:04 +0200

    Main view controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,10 +4,9 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
